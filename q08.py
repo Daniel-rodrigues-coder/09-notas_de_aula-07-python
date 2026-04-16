8bae7307d190f4c2639f3e8b08f81af0f87cdd90
Daniel-rodrigues-coder <rodrigues.araujo1@escolar.ifrn.edu.br>
2026-04-16 14:52:07 +0000

    perguntas respondidas

--- q08.py
+++ q08.py
@@ -1,1 +1,10 @@
-contador 
+contador = 1
+for contador in range(1, 20, 1  ):
+    if contador % 3 == 0 and contador % 5 == 0 :
+        print ("FizzBuzz ")
+    elif contador % 3 == 0 :
+        print ("Fizz") 
+    elif contador % 5 == 0:
+        print ("Buzz")
+    else:
+        print(f"{contador}")
